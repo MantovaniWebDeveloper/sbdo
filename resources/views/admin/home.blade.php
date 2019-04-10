315f75c6a2fb31faf0239798545651527730b836
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-04-10 14:51:53 +0200

    tolto il register

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -9,7 +9,7 @@
       </div>
       <div class="box">
         <div class="wrapTitle">
-          <a href="#"><h5 class="titleNav evento">Evento</h5></a>
+          <a href="#"><h5 class="titleNav evento">Evento<i class="fas fa-sort-down ml-1 pb-1"></i></h5></a>
         </div>
         <div id="1"class="dropDownMenu">
           <div class="wrapSubTitle">
@@ -19,7 +19,7 @@
       </div>
       <div class="box">
         <div class="wrapTitle">
-          <a href="#"><h5 class="titleNav locale">Locale</h5></a>
+          <a href="#"><h5 class="titleNav locale">Locale<i class="fas fa-sort-down ml-1 pb-1"></i></h5></a>
         </div>
         <div id="2"class="dropDownMenu">
           <div class="wrapSubTitle">
@@ -30,7 +30,7 @@
       </div>
       <div class="box">
         <div class="wrapTitle">
-          <a href=" "><h5 class="titleNav genere">Genere musicale</h5></a>
+          <a href=" "><h5 class="titleNav genere">Genere musicale <i class="fas fa-sort-down ml-1 pb-1"></i></h5></a>
 
         </div>
       </div>
@@ -83,35 +83,38 @@
         </div>
       </div>
       <div id="contentDashBoard">
-        <table class="table table-dark">
-          <thead>
-            <tr>
-              <th scope="col " class="text-center">idEvento</th>
-              <th scope="col" class="text-center">Evento</th>
-              <th scope="col " class="text-center">Data</th>
-              <th scope="col " class="text-center">Organizzatore</th>
-              <th scope="col " class="text-center">Modifica</th>
-              <th scope="col " class="text-center">cancella</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($eventi as  $evento)
+        <div class="table-responsive">
+          <table class="table table-dark">
+            <thead>
               <tr>
-                <th class="text-center">{{$evento->id}}</th>
-                <td class="text-center">{{$evento->nomeEvento}}</td>
-                <td class="text-center">{{$evento->data_svolgimento}}</td>
-                <td class="text-center">{{$evento->organizzatore}}</td>
-                <td class="text-center"><a href="{{ route('modificaEvento', $evento->id)}}"><button class="btn btn-success">Modica</button></a></td>
-                <td class="text-center">
-                  <form class="" action="{{ route('cancellaEvento', $evento->id) }}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <input class="btn btn-danger" type="submit" name="" value="Elimina">
-                </form>
+                <th scope="col " class="text-center">idEvento</th>
+                <th scope="col" class="text-center">Evento</th>
+                <th scope="col " class="text-center">Data</th>
+                <th scope="col " class="text-center">Organizzatore</th>
+                <th scope="col " class="text-center">Modifica</th>
+                <th scope="col " class="text-center">cancella</th>
               </tr>
-            @endforeach
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              @foreach ($eventi as  $evento)
+                <tr>
+                  <th class="text-center">{{$evento->id}}</th>
+                  <td class="text-center">{{$evento->nomeEvento}}</td>
+                  <td class="text-center">{{$evento->data_svolgimento}}</td>
+                  <td class="text-center">{{$evento->organizzatore}}</td>
+                  <td class="text-center"><a href="{{ route('modificaEvento', $evento->id)}}"><button class="btn btn-success">Modica</button></a></td>
+                  <td class="text-center">
+                    <form class="" action="{{ route('cancellaEvento', $evento->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input class="btn btn-danger" type="submit" name="" value="Elimina">
+                  </form>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+
       </div>
     </div>
   </div>
