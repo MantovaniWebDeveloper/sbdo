6094070247b0599b6a866f45cc431a6291c07da8
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-04-18 10:58:31 +0200

    corretto errore di visualizzazione sul search interno

--- resources/views/_partials/_searchInterno.blade.php
+++ resources/views/_partials/_searchInterno.blade.php
@@ -3,22 +3,25 @@
     <div id="internoOpacita" class="opacita">
       <div id="internoWrapForm"class="wrapForm">
         <div class="container">
-          <form class="" action="{{route('ricercaEventi')}}" method="POST">
-            @csrf
-            @method('POST')
+          <form class="" action="{{route('ricercaEventi')}}" method="get">
+            {{csrf_field()}}
+            @method('GET')
 
             <div class="form-row">
               <div class="inputMobile col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                <!-- <input type="text" class="form-control" placeholder="Inserisci la città" name="localita"> -->
-                <input id="citta-input"class="form-control" name="localita" list="cities" style="width:100%"/>
-                   <datalist id="cities" style="width:100%">
-                     {{--questo sarà riempito da handlebars--}}
-                   </datalist>              </div>
-              <div class="inputMobile col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                <input class="form-control"type="date" name="data" value="" id="dataGiorno">
+                <label for="citta" class="text-light">Seleziona la località:</label>
+                <select id="cities" class="citta-input" style="width:100%; "name="localita">
+
+                </select>
+                  <input id="lat" type="hidden" name="latitudine" value="">
+                  <input id="long" type="hidden" name="longitudine" value="">
               </div>
-              <div class="inputMobile col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                <input type="submit" class="btnInvioForm btn text-center btn-success" name="salva" value="Cerca">
+              <div class="inputMobile col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                <label for="citta" class="text-light">Seleziona la data:</label>
+                <input class="form-control"type="date" name="data" id="dataGiorno">
+              </div>
+              <div class="inputMobile col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 btnWrap">
+                <input type="submit" class="btnInvioForm btn text-center btn-success mb-3" name="salva" value="Cerca">
               </div>
             </div>
           </form>
@@ -30,8 +33,7 @@
 <!-- ZONA HANDLEBARS!!!-->
 <script id="listaCitta-template" type="text/x-handlebars-template">
   @{{#each this}}
-        <option class="elemento" data-id="@{{sigla}}" value="@{{provincia}}"></option>
+        <option class="elemento"  data-lat="@{{lat}}" data-long="@{{lng}}" value="@{{provincia}}">@{{provincia}}</option>
   @{{/each}}
-  </datalist>
-
+</script>
 </script>
