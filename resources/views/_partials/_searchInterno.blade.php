ad13f16a6f282365a0f7cb2a39dc0b46f0e3266c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-04-23 11:59:58 +0200

    inserito giusto datepicker

--- resources/views/_partials/_searchInterno.blade.php
+++ resources/views/_partials/_searchInterno.blade.php
@@ -16,7 +16,7 @@
               </div>
               <div class="inputMobile col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
                 <label for="citta" class="text-light">Seleziona la data:</label>
-                <input class="form-control"type="date" name="data" id="dataGiorno">
+                <input type="text" class="datepicker form-control" name="data" id="dataGiorno" autocomplete="off"/>
               </div>
               <div class="inputMobile col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 btnWrap">
                 <input type="submit" class="btnInvioForm btn text-center btn-success mb-3" name="salva" value="Cerca">
@@ -34,4 +34,17 @@
         <option class="elemento"  data-lat="@{{lat}}" data-long="@{{lng}}" value="@{{provincia}}">@{{provincia}}</option>
   @{{/each}}
 </script>
+<script type="text/javascript">
+
+$('.datepicker').datepicker({
+  dateFormat : "yy/mm/dd" ,
+  showAnim: "slideDown",
+  monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno', 'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+  monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu', 'Lug','Ago','Set','Ott','Nov','Dic'],
+  dayNames: ['Domenica','Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato'],
+  dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'],
+  dayNamesMin: ['Do','Lu','Ma','Me','Gio','Ve','Sa'],
+  firstDay: 1,
+  isRTL: false
+}) ;
 </script>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,10 +7,9 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/scripts/jquery.ui.datepicker-it.js"></script>
 
 
 
